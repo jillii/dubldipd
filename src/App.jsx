25375b7923caa7994f6a12d0995cc8471e75fd32
Jill <jho3292@gmail.com>
2024-01-28 18:50:09 -0500

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Window from './Window'
+
+function App() {
+
+  return (
+    <>
+      <div className="intro section">
+          <div className="site-container">
+              <Window id="presents" title="Alert" size="large" value="dubl dipd presents:" buttons={['Okay', 'Cancel']} />
+              <Window id="jill" title="Alert" size="small" value="jills birthday" buttons={['Ew']} />
+              <Window 
+                id="featuring" 
+                title="Notice" 
+                size="large" 
+                list={["Robbie Dean Rhodes", "M_tt D_ll", "Orange Peel Mystic", "Andrew Eckel"]}
+                />
+                <Window
+                  id="details"
+                  title="Info"
+                  tableTitle="Info"
+                  table={{
+                      "Date": "02 MARCH 2024",
+                      "Location": "Industry Lab",
+                      "Time": "6:00pm - 10:00pm",
+                      "Ages": "All",
+                      "BYOB": "Yes"
+                  }}
+                />
+          </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Window.jsx
@@ -0,0 +1,74 @@
+export default function Window(props) {
+    const id = props.id
+    const title = props.title
+    const value = props.value
+    const size = props.size
+    const list = props.list
+    const buttons = props.buttons
+    const tableTitle = props.tableTitle
+    const table = props.table
+    const arr = new Array(20).fill(' ')
+
+    return (        
+        <div id={id} className="window-container" data-size={size}>
+            {arr.map((x, index) =>
+                <div key={index} className="window magnet-poetry">
+                    <div className="title-bar">
+                        <div className="title-bar-text">{title}</div>
+                        <div className="title-bar-controls">
+                            <button aria-label="Minimize"></button>
+                            <button aria-label="Maximize"></button>
+                            <button aria-label="Close"></button>
+                        </div>
+                    </div>
+                    <div className="window-body">
+                        {value &&
+                            <p>{value}</p>
+                        }
+                        {list &&
+                            <ul className="tree-view">
+                                <li>
+                                    <details open>
+                                        <summary>With perfomances by:</summary>
+                                        <ul>
+                                            {list.map((item, index) =>
+                                                <li key={index}>{item}</li>
+                                            )}
+                                        </ul>
+                                    </details>
+                                </li>
+                            </ul>
+                        }
+                        {table &&
+                            <div className="sunken-panel" style={{height: 140, width: 200}}>
+                                <table className="interactive">
+                                    <thead>
+                                        <tr>
+                                        <th>Type</th>
+                                        <th>Value</th>
+                                        <th>Comment</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {Object.keys(table).map((key, index) => 
+                                            <tr key={index}>
+                                                <td>{key}</td>
+                                                <td>{table[key]}</td>
+                                                <td>n/a</td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
+                        }
+                        {buttons &&
+                            <section className="field-row" style={{justifyContent: "flex-end"}}>
+                                {buttons.map((button, index) => <button key={index}>{button}</button>)}
+                            </section>
+                        }
+                    </div>
+                </div> 
+            )}
+        </div>
+    );
+}
